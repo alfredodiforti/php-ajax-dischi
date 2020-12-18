c5c8f85e9670f4309fb1e0f5f961b5dcdd67391b
Alfredo <adf.palermo@icloud.com>
2020-12-18 18:20:03 +0100

    Millestone 2 completata (no bonus)

--- index.php
+++ index.php
@@ -1,51 +1,3 @@
-<?php
-
-    $database = [
-        [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
-            'genre' => 'Rock'
-        ],
-        [
-            'title' => 'Live at Wembley 86',
-            'author' => 'Queen',
-            'year' => 1992,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Ten\'s Summoner\'s Tales',
-            'author' => 'Sting',
-            'year' => 1993,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Steve Gadd band',
-            'author' => 'Steve Gadd Band',
-            'year' => 2018,
-            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
-            'genre' => 'Jazz'
-        ],
-        [
-            'title' => 'And Justice for All',
-            'author' => 'Metallica',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
-            'genre' => 'Metal'
-        ],
-        [
-            'title' => 'One more car, one more rider',
-            'author' => 'Eric Clapton',
-            'year' => 2002,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
-            'genre' => 'Rock'
-        ]
-    ];
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -63,18 +15,18 @@
  </header>
  <main>
      <div class="container flex">
-         <?php
-          foreach ($database as $details) { ?>
-           <div class="blackbox">
-               <img src="<?php echo $details['poster']?>" alt="<?php echo $details['title']?>">
-               <h3><?php echo $details['title']?></h3>
-               <span><?php echo $details['author']?></span>
-               <span><?php echo $details['year']?></span>
-               <span><?php echo $details['genre']?></span>              
+
+           <div v-for="details in datab" class="blackbox">
+               <img :src="details.poster" alt="datab.title">
+               <h3>{{details.title}}</h3>
+               <span>{{details.author}}</span>
+               <span>{{details.year}}</span>
+               <span>{{details.genre}}</span>              
            </div>
-          <?php } ?>
      </div>
  </main>
 </div>
+<!-- import js -->
+<script src="./dist/js/app.js"></script>
 </body>
 </html>
